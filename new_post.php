23e8a754279f587c5f963abf635f96e512ffdd36
HappyMaindreamer <46573113+HappyMaindreamer@users.noreply.github.com>
2019-05-09 19:22:54 +0200

    eee
    eee

---
+++ new_post.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+<link rel="stylesheet" href="style.css">
+
+<link rel="apple-touch-icon" href="file:///C:/Users/maindreamer/Desktop/Ashley/img/ashleymini.png">
+
+   <!-- Fügt das Logo hinzu -->
+
+<link rel="icon" href="#" type="image/png">
+<title>Maini Post | Neuste Posts</title>
+
+</head>
+
+<body>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+html {
+	font-family: 'Roboto', sans-serif;
+	background: white;
+	color: black;
+}
+
+a {
+	color: black;
+	text-decoration: none;
+}
+
+a:hover {
+	 color: #424242;
+}
+
+</style> 
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<img alt="Maini" src="img/maini black.png" width="100" height="49">
+
+
+
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="Post_Normal.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Blog.html">Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+
+
+
+
+
+
+</body>
+
+</html>
